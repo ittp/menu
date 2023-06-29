e9c1a478f939b599a63697dc786769218a33a815
ittp <61617660+ittp@users.noreply.github.com>
2023-06-29 01:54:09 +0000

    Update player.js

--- backend/routes/player.js
+++ backend/routes/player.js
@@ -7,5 +7,10 @@ let Player = ["GetActivePlayers", "PlayPause"];
 
 let pp = {"jsonrpc": "2.0", "method": "Player.PlayPause", "params": { "playerid": 0 }, "id": 1}
 
+let meta = axios.get('https://api.github.com/meta').then(response => response.data)
+
+console.log(metas)
+
 res.render("index", { title: "TV", count: 3, background: "", player: { ...pp, Player, });});
-      module.exports = router;
+
+module.exports = router;
