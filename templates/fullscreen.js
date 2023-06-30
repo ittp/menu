513c3f4e1c0e41eed6bcd35cc0fdec578d7ba2a9
tp <61617660+ittp@users.noreply.github.com>
2023-07-01 01:42:51 +0300

    Update fullscreen.js

--- templates/fullscreen.js
+++ templates/fullscreen.js
@@ -1,1 +1,19 @@
 
+let items = {
+
+
+}
+
+
+let coffee = `<svg width="1131" height="1097" viewBox="0 0 1131 1097" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_f_0_436)">
+<ellipse cx="565.142" cy="548.81" rx="542.958" ry="526.088" fill="black" fill-opacity="0.15"/>
+</g>
+<defs>
+<filter id="filter0_f_0_436" x="0.183594" y="0.722168" width="1129.92" height="1096.18" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+<feGaussianBlur stdDeviation="11" result="effect1_foregroundBlur_0_436"/>
+</filter>
+</defs>
+</svg>`
